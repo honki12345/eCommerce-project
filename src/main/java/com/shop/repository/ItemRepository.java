2084508dff8e079b49e023995b46dad00bc06b3d
Hoonki <iui4793@gmail.com>
2022-12-01 23:13:06 +0900

    ItemRepository & JPA test

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+}
