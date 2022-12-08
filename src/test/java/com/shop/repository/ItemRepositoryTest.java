520f56b9c292ed965cb44f0ff7bdfadb8780acfd
Hoonki <iui4793@gmail.com>
2022-12-08 21:51:50 +0900

    JPA example

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import java.time.LocalDateTime;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,9 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Autowired
     ItemRepository itemRepository;
@@ -81,6 +87,34 @@ class ItemRepositoryTest {
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
         printItems(itemList);
     }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+//    @Test
+//    @DisplayName("Querydsl 조회 테스트1")
+//    public void queryDslTest() {
+//        this.createItemList();
+//        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+//        QItem qItem = QItem.item;
+//    }
 
     public void printItems(List<Item> itemList) {
         for (Item item : itemList) {
